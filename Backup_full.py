515ab49b227f233e79134e22a698007a94de2fbd
Vladimir Vasilev <vvt182@gmail.com>
2016-01-27 14:24:03 +0300

    added parameters and some statistics

--- Backup_full.py
+++ Backup_full.py
@@ -4,33 +4,47 @@ import shutil
 import datetime
 import filecmp
 
-path_collection = []
-backup_from = r'D:\PY\tb'
-backup_to = r'D:\PY\backup'
+if len(sys.argv) != 3:
+    print('Not enough arguments!')
+    print('Usage: Backup_full.py sourse_dir target_dir')
+    #sys.exit()
+    backup_from = r'D:\PY\tb'
+    backup_to = r'D:\PY\backup'
+else:
+    backup_from = sys.argv[1]
+    backup_to = sys.argv[2]
+
+def source_count(pth):
+    dirs_src = 0
+    files_src = 0
+    ptree = os.walk(backup_from)
+    for dirpath, dirnames, filenames in ptree:
+        dirs_src = dirs_src + len(dirnames)
+        files_src = files_src + len(filenames)
+    return dirs_src, files_src
+
+print('Dirs in the source before backup: ',source_count(backup_from)[0])
+print('Files in the source before backup: ',source_count(backup_from)[1])
+
+files_to_be_copied = []
+files_copied = []
+files_identical = []
+files_different = []
+files_copy_error = []
 now_time = datetime.datetime.now().strftime('%d%m%Y-%H%M%S_full')
 dstfolder = os.path.join(backup_to, now_time)
 os.mkdir(dstfolder)
-print(now_time)
 ptree = os.walk(backup_from)
 item_in_path_to_backup = len(backup_from.split('\\'))
 ii=0
-files_identical = 0
-files_different = 0
 for dirpath, dirnames, filenames in ptree:
     ii=ii+1
-    print(ii)
-    print('Where we are now(dirpath) - ',dirpath)
-    print('Directories in current point(dirnames) - ',dirnames)
-    #print('Files in current point(filenames) - ',filenames)    #error if file not in ru_win encoding
     src_list_path = dirpath.split('\\')[item_in_path_to_backup:]
-    print('Folder in dirpath to be added to dstpath at this step - ',src_list_path)
     dstpath=dstfolder
     for folders in src_list_path:
         dstpath = os.path.join(dstpath, folders)   
         #dstpath = dstpath+'\\'+folders 
-    print("Where we are gonna copy - "+dstpath)
-    print('----------------------------------------------')
-  
+    
 #Making directories
     for dirs in dirnames:
         dircheck = os.path.join(dstpath, dirs)
@@ -46,32 +60,30 @@ for dirpath, dirnames, filenames in ptree:
     for fl in filenames:
         fullsrcpath = os.path.join(dirpath, fl)
         fulldstpath = os.path.join(dstpath, fl)
-        path_collection.append(fullsrcpath)
+        files_to_be_copied.append(fullsrcpath)
         try:
             shutil.copy2(fullsrcpath, fulldstpath)
+            files_copied.append(fullsrcpath)
         except IOError as e:
-            print('skipping', fullsrcpath, e.errno, e.strerror)
+            files_copy_error.append(fullsrcpath)
+            #'file name is too long' exception handling
+            if len(fulldstpath)>259:
+                print ('Skipping file: ', fulldstpath,' Destination path is to long -> ', len(fulldstpath))
+            else:
+                print('Skipping', fullsrcpath, e.errno, e.strerror)
             continue
         try:
             compare_result = filecmp.cmp(fullsrcpath, fulldstpath, shallow=False)
             if compare_result:
-                #print('Source and destination files are identical')
-                files_identical = files_identical+1
+                files_identical.append(fullsrcpath)
             else:
-                #print('Source and destination files are DIFFERENT')
-                files_different = files_different+1
+                files_different.append(fulldstpath)
         except IOError as e:
             print('Comparsion ',fullsrcpath, ' and ', fulldstpath, ' failed!', e.errno, e.strerror)
    
-#print(path_collection) #error if file not in ru_win encoding
-print('Files copied - ', len(path_collection))
-print('Files identical - ', files_identical)
-print('Files different - ', files_different)   
+print('Files ready to be copied - ', len(files_to_be_copied))
+print('Files copied - ', len(files_copied))
+print('Files not copied - ', len(files_copy_error))
+print('Files identical - ', len(files_identical))
+print('Files different - ', len(files_different))   
     
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print('Usage: Backup_full sourse_dir target_dir')
-        sys.exit()
-    else:
-        backup_from = sys.argv[0]
-        backup_to = sys.argv[1]
--- test.py
+++ test.py
@@ -3,34 +3,11 @@ import shutil
 import datetime
 import filecmp
 
+strcmd = r'd:\py\backup\27012016-132510_full\testtesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetset.txt'
+print('STR_cmd -> ',len(strcmd))
+print(strcmd)
+strscript = 'testtesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetsetsetsettesttesttsetset.txt'
+print('STR_script -> ',len(strscript))
 
-def func():
-    print("func() in one.py")
-
-print("top-level in one.py")
-
-if __name__ == "__main__":
-    print("one.py is being run directly")
-else:
-    print("one.py is being imported into another module")
-
-"""
-fullsrcpath = 'D:/PY/win.txt'
-fulldstpath = 'D:/PY/backup/25012016-111908_full/Folder1/Папка3/win.txt'
-
-try:
-	compare_result = filecmp.cmp(fullsrcpath, fulldstpath, shallow=False)
-	if compare_result:
-		print('Source and destination files are identical')
-		
-	else:
-		print('Source and destination files are DIFFERENT')
-		
-except os.error:
-   	print('Comparsion ',fullsrcpath, ' and ', fulldstpath, ' failed!', sys.exc_info()[0])
-
-print(os.path.getsize('D:/PY/win.txt'))
-print(os.path.getsize('D:/PY/backup/25012016-111908_full/Folder1/Папка3/win.txt'))
-print(os.path.getmtime('D:/PY/win.txt'))
-print(os.path.getmtime('D:/PY/backup/25012016-111908_full/Folder1/Папка3/win.txt'))
-"""
+bstr = b'byte'
+print(bstr.encode('utf-8'))
